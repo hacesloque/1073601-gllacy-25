70aebb3ad35615f13311b719371236bfc11fd8e0
hacesloque <aaaaaagrrrrrr@gmail.com>
2019-07-02 16:24:10 +0300

    интерактивная карта

---
+++ js/googlemaps.js
@@ -0,0 +1,16 @@
+var map;
+
+function initMap() {
+  (map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 59.939348, lng: 30.329308 },
+    zoom: 16,
+    disableDefaultUI: true
+  })),
+    (image ="../img/decor/pin.png"),
+    (marker = new google.maps.Marker({
+      position: { lat: 59.938878, lng: 30.322994 },
+      map: map,
+      icon: image,
+      animation: google.maps.Animation.DROP
+    }));
+}
